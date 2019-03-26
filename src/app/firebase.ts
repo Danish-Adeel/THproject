1894afc62b8bdab4589ca1a378ab5b52660628b3
Danish Adeel <danishadeel95@yahoo.com>
2019-03-26 18:17:07 +0100

    first commit

---
+++ src/app/firebase.ts
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyAlGmFiTiv76ktBw1TXIs3UwuBeArsF5YA",
+  authDomain: "thproject-dba2f.firebaseapp.com",
+  databaseURL: "https://thproject-dba2f.firebaseio.com",
+  projectId: "thproject-dba2f",
+  storageBucket: "thproject-dba2f.appspot.com",
+  messagingSenderId: "1034349897374"
+};
+
+export default config;
