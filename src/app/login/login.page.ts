dac091fdb024429593d24863540ef5ada4d5828e
AliFarooq0072 <blueprintmattmorgan@gmail.com>
2019-03-27 01:44:07 +0500

    Login page background

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -8,8 +8,8 @@ import { auth } from "firebase/app";
   styleUrls: ["./login.page.scss"]
 })
 export class LoginPage implements OnInit {
-  username: string = "";
-  password: string = "";
+  username: string;
+  password: string;
   constructor(public afAuth: AngularFireAuth) {}
 
   ngOnInit() {}
